0a4ae38ce289de3bd2eb816691dcaf984b9c9bc9
3kybika <vladislav.alehin@mail.ru>
2018-02-09 00:17:23 +0300

    Create euclid.js

---
+++ source/euclid.js
@@ -0,0 +1,28 @@
+ 'use strict';
+ 
+const euclid = function () {
+	
+	if ( arguments.length === 0){
+		// Wrong number of parameters
+		return undefined;
+	}
+	
+	let divider = Math.min.apply(Math, arguments);
+  
+	if (divider ===0){
+		return null;
+	}
+	
+    for (let i = 0; i < arguments.length; ++i) { 
+      let residue = arguments[i];
+
+      // Euclid's algorithm for finding the GCD
+      while (divider !== 0){
+        [divider, residue] = [residue % divider , divider]
+      }
+
+      divider = residue;
+    }
+	
+	return divider;
+};
